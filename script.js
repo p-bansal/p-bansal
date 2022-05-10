dcc9a750eb04b264037066c949e96de00f9f8a7e
p-bansal <105261891+p-bansal@users.noreply.github.com>
2022-05-09 22:48:13 -0600

    Added pukhi.ca files from repl.it

---
+++ script.js
@@ -0,0 +1,15 @@
+function openSite(){
+  
+  document.getElementById("codeDiv").style.display = "none";
+  document.getElementById("buttonholder").style.display = "none";
+  document.getElementById("projectholder").style.display = "flex";
+  var projects = document.getElementsByClassName("projects");
+  var counter = 1;
+  for(el of projects){
+    el.classList.add("animated");
+  }
+}
+
+function output(){
+  console.log("Out");
+}
